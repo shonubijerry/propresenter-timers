f96a5d431738ce13963431eca2a7f97a74ee111d
shonubijerry <korede@verifi.ng>
2025-09-20 04:21:40 +0100

    fix type and build

--- src/app/api/timers/[id]/[operation]/route.ts
+++ src/app/api/timers/[id]/[operation]/route.ts
@@ -1,26 +1,24 @@
-import { NextResponse } from "next/server";
-
 export async function GET(
   _: Request,
-  { params }: { params: { id: string; operation: string } }
+  { params }: { params: Promise<{ id: string; operation: string }> }
 ) {
-  const { id, operation } = await params;
+  const { id, operation } = await params
 
   return fetch(
     `${process.env.NEXT_PUBLIC_PROPRESENTER_BASE_URL}/v1/timer/${id}/${operation}`
-  );
+  )
 }
 
 export async function PUT(
   _: Request,
-  { params }: { params: { id: string; operation: string } }
+  { params }: { params: Promise<{ id: string; operation: string }> }
 ) {
-  const { id, operation } = await params;
+  const { id, operation } = await params
 
   return fetch(
     `${process.env.NEXT_PUBLIC_PROPRESENTER_BASE_URL}/v1/timer/${id}/${operation}`,
     {
-      method: "PUT",
+      method: 'PUT',
     }
-  );
+  )
 }
