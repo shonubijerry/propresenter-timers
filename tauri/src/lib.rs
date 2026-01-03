f0c627c1ed726971870c16a5d4a91e0d87a5bc44
shonubijerry <korede@verifi.ng>
2026-01-03 17:34:54 +0000

    enable plugin log for prod

--- tauri/src/lib.rs
+++ tauri/src/lib.rs
@@ -5,17 +5,14 @@ mod migrations;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .plugin(
+      tauri_plugin_log::Builder::default()
+        .level(log::LevelFilter::Debug)
+        .build(),
+    )
     .plugin(tauri_plugin_sql::Builder::new().build())
     .setup(|app| {
       // ----- Plugins -----
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Debug)
-            .build(),
-        )?;
-      }
-
       #[cfg(desktop)]
       app
         .handle()
